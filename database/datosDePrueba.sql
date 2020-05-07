4e82b1d16832574b10fea38d7ada8cd00206341d
Ubuntu <a18neigutcha@inspedralbes.cat>
2020-05-07 11:54:45 +0200

    correccion de los datos insertados en la base de datos

--- database/datosDePrueba.sql
+++ database/datosDePrueba.sql
@@ -1,48 +1,28 @@
-/*
-	Datos de prueba para el desarrollo
-*/
-
 USE RE_FIND;
  
-/*
-	Datos de usuario por defecto
-*/
-
 INSERT INTO USUARIO VALUES 
 	(1,'Ryan','Ray','Ryan@gmail.com','Ryan','password'),
     (2,'Joe','McMilan','Joe@gmail.com','Joe','password'),
     (3,'Jhon','Carter','Jhon@gmail.com','Jhon','password');
 
-/*
-	Datos de lugares ficticios
-*/
 
 INSERT INTO LUGAR VALUES
 	(1,'Bar Bacardi','Calle ricart 26',' ',' ','Bar con unos buenos cocteles',5),
     (2,'Plaza españa','Plaza españa',' ',' ','Lugar turistico muy turistico',7),
     (3,'Restaurante harryBer','Calle augusto 3ro',' ',' ','Restaurante habientado en Harry Potter',9);
     
-/*
-	Datos de noticias de algun lugar
-*/
 
 INSERT INTO NOTICIA VALUES
 	(1,'Promocion 2x1','Hoy tenemos una promocion de dos por uno en margaritas',1),
     (2,'Hay un concierto esta noche','Hoy habra un concierto a las 12 pm',2),
     (3,'Hoy tenemos un famoso invitado','Ventra la autora J. K. Rowling de 2 pm a 6 pm',3);
     
-/*
-	Categorias
-*/
 
 INSERT INTO CATEGORIA VALUES
 	(1,'Restaurant'),
     (2,'Lugar turistico'),
     (3,'Ambientacion');
     
-/*
-	CATEGORIA_LUGAR
-*/
 
 INSERT INTO CATEGORIA_LUGAR VALUES
 	(1,1),
@@ -69,18 +49,12 @@ INSERT INTO VISITA_PENDIENTE VALUES
     (2,2),
     (3,3);
     
-/*
-	Comentarios
-*/
 
 INSERT INTO COMENTARIO VALUES
 	(1,1,1,'Un lugar interesante',4),
     (2,2,2,'Es un lugar muy concurrido',3),
     (3,3,3,'Me encanta el ambiente',9);
-    
-/*
-	Denuncias
-*/
+
 
 INSERT INTO DENUNCIA VALUES
 	(1,2,1,'Contenido inapropiado'),
